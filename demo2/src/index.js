31b16344c55af360c222944f911c3976e84d2309
liangwei02 <liangwei02@wangcaigu.com>
2018-01-26 16:51:26 +0800

    demo2

---
+++ demo2/src/index.js
@@ -0,0 +1,21 @@
+import _ from 'lodash';
+import './style.css';
+import Avatar from './avatar.png';
+
+function component() {
+    var element = document.createElement('div');
+
+    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+
+    element.classList.add('hello');
+
+    var myAvatar = new Image();
+
+    myAvatar.src = Avatar;
+
+    element.appendChild(myAvatar);
+
+    return element;
+}
+
+document.body.appendChild(component());
